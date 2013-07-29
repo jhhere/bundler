847d21c4dcabb5abaeeef069dc3232ac3593fbd5
joyicecloud <joyicecloud@gmail.com>
2013-07-29 14:56:58 -0700

    blah

--- spec/other/cli_dispatch_spec.rb
+++ spec/other/cli_dispatch_spec.rb
@@ -1,5 +1,4 @@
 require "spec_helper"
-require 'test/unit'
 
 describe "Argument Error when typing bundle i" do
 
@@ -11,14 +10,5 @@ describe "Argument Error when typing bundle i" do
   it "should display a friendly message to the world" do
     bundle "i"
     expect(out).to match(/Did you make a typo\? It should be bundle init, bundle inject, or bundle install/)
-  end
-end
-
-class TestCLI < Test::Unit::TestCase
-  def test_dbundle_i
-    begin
-    rescue ArgumentError => e
-    end
-    assert_equal true, e.is_a?(ArgumentError), "Should be a Argument Error"
   end
 end
