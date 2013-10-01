e4181c981350417f59876172d93cd88327f5d765
joyicecloud <joyicecloud@gmail.com>
2013-09-30 21:29:29 -0700

    Create rspec test that prints to lockfile

---
+++ spec/warn-bundler-version-older-than-in-lockfile_spec.rb
@@ -0,0 +1,45 @@
+require "spec_helper"
+
+describe "lockfile" do
+  include Bundler::GemHelpers
+
+  it "prints out the Bundler version that generated the lockfile" do
+
+    install_gemfile <<-G
+      source "file://#{gem_repo1}"
+
+      gem "rails"
+    G
+
+    lockfile_should_be <<-G
+      GEM
+        remote: file:#{gem_repo1}/
+        specs:
+          actionmailer (2.3.2)
+            activesupport (= 2.3.2)
+          actionpack (2.3.2)
+            activesupport (= 2.3.2)
+          activerecord (2.3.2)
+            activesupport (= 2.3.2)
+          activeresource (2.3.2)
+            activesupport (= 2.3.2)
+          activesupport (2.3.2)
+          rails (2.3.2)
+            actionmailer (= 2.3.2)
+            actionpack (= 2.3.2)
+            activerecord (= 2.3.2)
+            activeresource (= 2.3.2)
+            rake (= 10.0.2)
+          rake (10.0.2)
+
+      PLATFORMS
+        #{generic(Gem::Platform.local)}
+
+      DEPENDENCIES
+        rails
+
+      BUNDLER
+    G
+
+  end
+end
