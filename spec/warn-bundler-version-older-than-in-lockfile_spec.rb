acf250d0814c41c98f1f9c74fb092c7ff5588f57
joyicecloud <joyicecloud@gmail.com>
2013-10-02 11:20:17 -0700

    Add bundler version to first spec test

--- spec/warn-bundler-version-older-than-in-lockfile_spec.rb
+++ spec/warn-bundler-version-older-than-in-lockfile_spec.rb
@@ -39,7 +39,47 @@ describe "lockfile" do
         rails
 
       BUNDLER
+        #{Bundler::VERSION}
     G
+
+  end
+
+  it "warns if the bundler version is older than the version that created" do
+    gemfile <<-G
+      source "file://#{gem_repo1}"
+
+      gem "rails"
+    G
+
+    lockfile <<-L
+      GEM
+        remote: file:#{gem_repo1}/
+        specs:
+          actionmailer (2.3.2)
+            activesupport (= 2.3.2)
+          actionpack (2.3.2)
+            activesupport (= 2.3.2)
+          activerecord (2.3.2)
+            activesupport (= 2.3.2)
+          activeresource (2.3.2)
+            activesupport (= 2.3.2)
+          activesupport (2.3.2)
+          rails (2.3.2)
+            actionmailer (= 2.3.2)
+            actionpack (= 2.3.2)
+            activerecord (= 2.3.2)
+            activeresource (= 2.3.2)
+            rake (= 10.0.2)
+          rake (10.0.2)
+
+      PLATFORMS
+        #{generic(Gem::Platform.local)}
+
+      DEPENDENCIES
+        rails
+
+      BUNDLER
+    L
 
   end
 end
